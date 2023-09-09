80a915790cbb3c5654afa4de02c433de700f958c
justantvish <justantvish@gmail.com>
2023-09-09 18:18:55 +0300

    useEffect dependencies fix

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -11,24 +11,25 @@ export const useFetch = (url: string): fetchResponse => {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<any>();
 
-    const fetchData = async () => {
-        setLoading(true);
-
-        try {
-            const res = await fetch(url);
-            const json = await res.json();
-
-            setLoading(false);
-            setData(json);
-            setError(null);
-        } catch(error) {
-            setLoading(false);
-            setError('Could not fetch the data');
-        }
-    }
     useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+
+            try {
+                const res = await fetch(url);
+                const json = await res.json();
+
+                setLoading(false);
+                setData(json);
+                setError(null);
+            } catch(error) {
+                setLoading(false);
+                setError('Could not fetch the data');
+            }
+        }
+    
         fetchData();
-    }, []);
+    }, [url]);
 
     return { data, loading, error };
 };
